f877e32bcd4b16f7128de01146bbd3976e511833
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-12 22:20:50 -0400

    simple routing

--- server.py
+++ server.py
@@ -12,16 +12,31 @@ def server():
     s.bind(addr)
     s.listen(1)
 
-    print("starting server on port:", 80)
+    print('starting server on port:', 80)
 
     while True:
         c, addr = s.accept()
         c.settimeout(TIMEOUT)
 
         req = str(c.recv(MAX_BODY_SIZE))
-        print("recieved connection from: ", addr)
+        print('recieved connection from: ', addr)
         print(req)
-        c.send('HTTP/1.1 200 Ok\n')
-        c.send('Content-Type: application/json\n')
+
+        parsed_req = parse_http(req)
+        routes(c, parsed_req)
         c.close()
+
+def parse_http(raw):
+    split = raw.lstrip("b'").rstrip("'").split()
+    return {
+        'method': split[0],
+        'path': split[1]
+    }
+
+def routes(c, req):
+
+    if req['method'] == 'GET' and req['path'] == '/health':
+        c.send('HTTP/1.1 200 Ok\n')
+    else:
+        c.send('HTTP/1.1 404 NotFound\n')
 
