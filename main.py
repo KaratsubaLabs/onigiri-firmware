419832507d621dcf618e22cd125b16b9965631e1
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-09-12 20:58:31 -0400

    network connection sequence

--- main.py
+++ main.py
@@ -3,10 +3,43 @@
 from machine import Pin
 import time
 
-pin5 = Pin(5, Pin.OUT)
-for i in range(0, 10):
-    pin5.on()
-    time.sleep(1)
-    pin5.off()
-    time.sleep(1)
+import network
+import config
 
+# amount of times will attempt to connect to internet before failing
+NETWORK_RETRIES = 10 
+
+# NETWORK CONNECTION SEQUENCE
+#   - blinking when attempting to connect to internet
+#   - LED stays on when connection is established
+#   - LED says off when failed maximum number of network connection retries
+def network_connect():
+    pin5 = Pin(5, Pin.OUT)
+
+    sta_if = network.WLAN(network.STA_IF) # station interface
+    ap_if = network.WLAN(network.AP_IF)   # access point interface
+
+    sta_if.active(True)
+    ap_if.active(False)
+
+    time.sleep(3)
+    for i in range(NETWORK_RETRIES):
+        for j in range(5):
+            pin5.on()
+            time.sleep(0.1)
+            pin5.off()
+            time.sleep(0.1)
+
+        sta_if.connect(config.SSID, config.PSK)
+        time.sleep(1)
+        if sta_if.isconnected():
+            break
+
+    if sta_if.isconnected():
+        pin5.on()
+        return True
+    else:
+        pin5.off()
+        return False
+
+network_connect()
