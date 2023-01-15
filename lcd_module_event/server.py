654c4fcf3a905ed7167b47c2d6d4c11d874b7250
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-01-15 01:35:29 -0500

    sound module

--- lcd_module_event/server.py
+++ lcd_module_event/server.py
@@ -1,13 +1,19 @@
+PORT = 80
+TIMEOUT = 20
+MAX_BODY_SIZE = 1024
 
+import lcd
+
+lcd.init()
 
 '''
 API
 
-turn buzzer on
-{ state: 'on' }
+write to line 1
+{ mode: 'write', text: 'hello world', line: 0 }
 
-turn buzzer off
-{ state: 'off' }
+clear
+{ mode: 'clear' }
 
 '''
 
@@ -15,8 +21,7 @@ import socket
 import network
 import ujson
 import config
-import time
-from machine import Pin, PWM
+from machine import Pin
 
 TIMEOUT = 20
 MAX_BODY_SIZE = 4096
@@ -26,7 +31,7 @@ PORT = 80
 HOST = 'home.karatsubalabs.com'
 
 def server():
-    
+
     pin_18 = Pin(18, Pin.OUT)
 
     sta_if = network.WLAN(network.STA_IF)
@@ -40,7 +45,6 @@ def server():
     payload = 'GET /v1beta/event/device/' + config.DEVICE_NAME + ' HTTP/1.1\nHost: ' + HOST +'\n\n'
     print(payload)
     r = s.send(payload)
-
     while True:
 
         req = str(s.recv(4096))[2:-1]
@@ -58,12 +62,28 @@ def server():
             print('json exception '+str(e))
             continue
 
-        if parsed['state'] == 'on':
-            pin_18.on()
-        elif parsed['state'] == 'off':
-            pin_18.off()
+        # TODO hella hacky
+        try:
+            print(parsed['mode'] + ' ' + parsed['text'] + ' ' + parsed['line'])
+        except:
+            continue
+
+        if parsed['mode'] == 'write':
+            lcd.set_line(line)
+        
+            line = lcd.LINE_ONE
+            if parsed['line'] == '1':
+                line = lcd.LINE_ONE
+            elif parsed['line'] == '2':
+                line = lcd.LINE_TWO
+
+            lcd.set_line(line)
+            lcd.display(parsed['text'])
+        elif parsed['mode'] == 'clear':
+            pass
         else:
-            print('invalid state: ' + parsed['state'])
+            print('invalid mode: ' + parsed['mode'])
 
     s.close()
+
     
